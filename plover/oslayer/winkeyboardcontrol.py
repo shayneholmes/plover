fa1234e0f2960a52bc19d73322b0889b6f30cc8c
Ted Morin <morinted@gmail.com>
2014-10-29 12:03:22 -0400

    Rewrite Windows KeyboardEmulation, improve DevReadme
    This commit is for the Windows distribution. It:
    
    - Replaces the use of SendKeys with the lower-level `win32api.keybd_event`
    	+ Now keys are emulated down and up like on Linux and OSX
    	+ Works on more applications, like:
    		* Z-Type
    		* 10FastFingers
    		* GTK+ Programs (Pidgin)
    		* ...virtually all programs
    	+ Support for more keys
    		* Window key support (Super_L/R)
    		* Media keys
    			- Vol_Up
    			- Vol_Down
    			- Vol_Mute
    			- Media_Play_Pause
    			- Media_Next
    			- Media_Prev
    			- Media_Stop
    			- Sleep
    		* F1 through F24
    		* Caps_Lock (affects output)
    		* Help, Num_Lock, etc.
    - Update Windows DevReadme to address complications when setting up.
      Remove ambiguity.

--- plover/oslayer/winkeyboardcontrol.py
+++ plover/oslayer/winkeyboardcontrol.py
@@ -20,9 +20,9 @@ import functools
 import pyHook
 import pythoncom
 import threading
+import collections
 import win32api
 import win32con
-from pywinauto.SendKeysCtypes import SendKeys as _SendKeys
 
 def SendKeys(s):
     _SendKeys(s, with_spaces=True, pause=0)
@@ -39,6 +39,160 @@ KEY_TO_ASCII = {
     46: 'c', 47: 'v', 48: 'b', 49: 'n', 50: 'm', 51: ',', 
     52: '.', 53: '/', 57: ' ',
 }
+
+# "Narrow python" unicode objects store chracters in UTF-16 so we 
+# can't iterate over characters in the standard way. This workaround 
+# let's us iterate over full characters in the string.
+def characters(s):
+    encoded = s.encode('utf-32-be')
+    characters = []
+    for i in xrange(len(encoded)/4):
+        start = i * 4
+        end = start + 4
+        character = encoded[start:end].decode('utf-32-be')
+        yield character
+"""
+NOTE: This is copied from the osxkeyboardcontrol.py and modified
+for Windows using:
+
+http://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
+"""
+
+# This mapping only works on keyboards using the ANSI standard layout. Each
+# entry represents a sequence of keystrokes that are needed to achieve the
+# given symbol. First, all keydown events are sent, in order, and then all
+# keyup events are send in reverse order.
+KEYNAME_TO_KEYCODE = collections.defaultdict(list, {
+    # Adding media controls for windows
+    'Vol_Mute': [0xAD], 'Vol_Down': [0xAE], 'Vol_Up': [0xAF],
+    'Media_Next': [0xB0], 'Media_Prev': [0xB1], 'Media_Stop': [0xB2],
+    'Media_Play_Pause': [0xB3], 'Sleep': [0x5F],
+
+    # The order follows that of the plover guide.
+    '0': [0x30], '1': [0x31], '2': [0x32], '3': [0x33], '4': [0x34], '5': [0x35],
+    '6': [0x36], '7': [0x37], '8': [0x38], '9': [0x39],
+
+    'a': [0x41], 'b': [0x42], 'c': [0x43], 'd': [0x44], 'e': [0x45], 'f': [0x46], 'g': [0x47],
+    'h': [0x48], 'i': [0x49], 'j': [0x4A], 'k': [0x4B], 'l': [0x4C], 'm': [0x4D], 'n': [0x4E],
+    'o': [0x4F], 'p': [0x50], 'q': [0x51], 'r': [0x52], 's': [0x53], 't': [0x54], 'u': [0x55],
+    'v': [0x56], 'w': [0x57], 'x': [0x58], 'y': [0x59], 'z': [0x5A],
+
+    # Same as above, plus Shift_R for capital
+    'A': [0xA1, 0x41], 'B': [0xA1, 0x42], 'C': [0xA1, 0x43], 'D': [0xA1, 0x44], 'E': [0xA1, 0x45], 'F': [0xA1, 0x46], 'G': [0xA1, 0x47],
+    'H': [0xA1, 0x48], 'I': [0xA1, 0x49], 'J': [0xA1, 0x4A], 'K': [0xA1, 0x4B], 'L': [0xA1, 0x4C], 'M': [0xA1, 0x4D], 'N': [0xA1, 0x4E],
+    'O': [0xA1, 0x4F], 'P': [0xA1, 0x50], 'Q': [0xA1, 0x51], 'R': [0xA1, 0x52], 'S': [0xA1, 0x53], 'T': [0xA1, 0x54], 'U': [0xA1, 0x55],
+    'V': [0xA1, 0x56], 'W': [0xA1, 0x57], 'X': [0xA1, 0x58], 'Y': [0xA1, 0x59], 'Z': [0xA1, 0x5A],
+
+    'Alt_L': [0x12], 'Alt_R': [0x12], 'Control_L': [0xA2], 'Control_R': [0xA3],
+    'Hyper_L': [], 'Hyper_R': [], 'Meta_L': [], 'Meta_R': [],
+    'Shift_L': [0xA0], 'Shift_R': [0xA1], 'Super_L': [0x5B], 'Super_R': [0x5C],
+
+    'Caps_Lock': [0x14], 'Num_Lock': [0x90], 'Scroll_Lock': [0x91], 'Shift_Lock': [],
+
+    'Return': [0x0D], 'Tab': [0x09], 'BackSpace': [0x08], 'Delete': [0x2E],
+    # Note I selected PRINT key, there is also a PRINT SCREEN key
+    'Escape': [0x1B], 'Break': [0x03], 'Insert': [0x2D], 'Pause': [0x13], 'Print': [0x2A],
+    'Sys_Req': [],
+
+    'Up': [0x26], 'Down': [0x28], 'Left': [0x25], 'Right': [0x27],
+    'Page_Up': [0x21],
+    'Page_Down': [0x22], 'Home': [0x24], 'End': [0x23],
+
+    'F1': [0x70], 'F2': [0x71], 'F3': [0x72], 'F4': [0x73], 'F5': [0x74], 'F6': [0x75],
+    'F7': [0x76], 'F8': [0x77], 'F9': [0x78], 'F10': [0x79], 'F11': [0x7A],
+    'F12': [0x7B], 'F13': [0x7C], 'F14': [0x7D], 'F15': [0x7E], 'F16': [0x7F],
+    'F17': [0x80], 'F18': [0x81], 'F19': [0x82], 'F20': [0x83], 'F21': [0x84], 'F22': [0x85],
+    'F23': [0x86], 'F24': [0x87], 'F25': [], 'F26': [], 'F27': [], 'F28': [],
+    'F29': [], 'F30': [], 'F31': [], 'F32': [], 'F33': [], 'F34': [],
+    'F35': [],
+
+    'L1': [], 'L2': [], 'L3': [], 'L4': [], 'L5': [], 'L6': [],
+    'L7': [], 'L8': [], 'L9': [], 'L10': [],
+
+    'R1': [], 'R2': [], 'R3': [], 'R4': [], 'R5': [], 'R6': [],
+    'R7': [], 'R8': [], 'R9': [], 'R10': [], 'R11': [], 'R12': [],
+    'R13': [], 'R14': [], 'R15': [],
+
+    'KP_0': [0x60], 'KP_1': [0x61], 'KP_2': [0x62], 'KP_3': [0x63], 'KP_4': [0x64],
+    'KP_5': [0x65], 'KP_6': [0x66], 'KP_7': [0x67], 'KP_8': [0x68], 'KP_9': [0x69],
+    'KP_Add': [0xA1, 0xBB], 'KP_Begin': [], 'KP_Decimal': [0x6E], 'KP_Delete': [0x2E],
+    'KP_Divide': [0x6F], 'KP_Down': [], 'KP_End': [], 'KP_Enter': [0x0D],
+    'KP_Equal': [0xBB], 'KP_F1': [], 'KP_F2': [], 'KP_F3': [], 'KP_F4': [],
+    'KP_Home': [], 'KP_Insert': [], 'KP_Left': [], 'KP_Multiply': [0x6A],
+    'KP_Next': [], 'KP_Page_Down': [], 'KP_Page_Up': [], 'KP_Prior': [],
+    'KP_Right': [], 'KP_Separator': [], 'KP_Space': [], 'KP_Subtract': [0x6D],
+    'KP_Tab': [], 'KP_Up': [],
+
+    'ampersand': [0xA1, 0x37], 'apostrophe': [0xDE], 'asciitilde': [0xA1, 0xC0],
+    'asterisk': [0xA1, 0x38], 'at': [0xA1, 0x32], 'backslash': [0xDC],
+    'braceleft': [0xA1, 0xDB], 'braceright': [0xA1, 0xDD], 'bracketleft': [0xDB],
+    'bracketright': [0xDD], 'colon': [0xA1, 0xBA], 'comma': [0xBC], 'division': [],
+    'dollar': [0xA1, 0x34], 'equal': [0xBB], 'exclam': [0xA1, 0x31], 'greater': [0xA1, 0xBE],
+    'hyphen': [], 'less': [0xA1, 0xBC], 'minus': [0xBD], 'multiply': [],
+    'numbersign': [0xA1, 0x33], 'parenleft': [0xA1, 0x39], 'parenright': [0xA1, 0x30],
+    'percent': [0xA1, 0x35], 'period': [0xBE], 'plus': [0xA1, 0xBB],
+    'question': [0xA1, 0xBF], 'quotedbl': [0xA1, 0xDE], 'quoteleft': [],
+    'quoteright': [], 'semicolon': [0xBA], 'slash': [0xBF], 'space': [0x20],
+    'underscore': [0xA1, 0xBD],
+
+    # Many of these are possible but I haven't filled them in because it's a
+    # pain to do so. Others are only possible with multiple keypresses and
+    # releases making it impossible to do as a keycombo.
+
+    'AE': [], 'Aacute': [], 'Acircumflex': [], 'Adiaeresis': [],
+    'Agrave': [], 'Aring': [], 'Atilde': [], 'Ccedilla': [], 'Eacute': [],
+    'Ecircumflex': [], 'Ediaeresis': [], 'Egrave': [], 'Eth': [],
+    'ETH': [], 'Iacute': [], 'Icircumflex': [], 'Idiaeresis': [],
+    'Igrave': [], 'Ntilde': [], 'Oacute': [], 'Ocircumflex': [],
+    'Odiaeresis': [], 'Ograve': [], 'Ooblique': [], 'Otilde': [],
+    'THORN': [], 'Thorn': [], 'Uacute': [], 'Ucircumflex': [],
+    'Udiaeresis': [], 'Ugrave': [], 'Yacute': [],
+
+    'ae': [], 'aacute': [], 'acircumflex': [], 'acute': [],
+    'adiaeresis': [], 'agrave': [], 'aring': [], 'atilde': [],
+    'ccedilla': [], 'eacute': [], 'ecircumflex': [], 'ediaeresis': [],
+    'egrave': [], 'eth': [], 'iacute': [], 'icircumflex': [],
+    'idiaeresis': [], 'igrave': [], 'ntilde': [], 'oacute': [],
+    'ocircumflex': [], 'odiaeresis': [], 'ograve': [], 'oslash': [],
+    'otilde': [], 'thorn': [], 'uacute': [], 'ucircumflex': [],
+    'udiaeresis': [], 'ugrave': [], 'yacute': [], 'ydiaeresis': [],
+
+    'cedilla': [], 'diaeresis': [], 'grave': [0xC0], 'asciicircum': [0xA1, 0x36],
+    'bar': [0xA1, 0xDC], 'brokenbar': [], 'cent': [], 'copyright': [],
+    'currency': [], 'degree': [], 'exclamdown': [], 'guillemotleft': [],
+    'guillemotright': [], 'macron': [], 'masculine': [], 'mu': [],
+    'nobreakspace': [], 'notsign': [], 'onehalf': [], 'onequarter': [],
+    'onesuperior': [], 'ordfeminine': [], 'paragraph': [],
+    'periodcentered': [], 'plusminus': [], 'questiondown': [],
+    'registered': [], 'script_switch': [], 'section': [], 'ssharp': [],
+    'sterling': [], 'threequarters': [], 'threesuperior': [],
+    'twosuperior': [], 'yen': [],
+
+    'Begin': [], 'Cancel': [], 'Clear': [], 'Execute': [], 'Find': [],
+    'Help': [0x2F], 'Linefeed': [], 'Menu': [], 'Mode_switch': [0x1F],
+    'Multi_key': [], 'MultipleCandidate': [], 'Next': [],
+    'PreviousCandidate': [], 'Prior': [], 'Redo': [], 'Select': [],
+    'SingleCandidate': [], 'Undo': [],
+
+    'Eisu_Shift': [], 'Eisu_toggle': [], 'Hankaku': [], 'Henkan': [],
+    'Henkan_Mode': [], 'Hiragana': [], 'Hiragana_Katakana': [],
+    'Kana_Lock': [], 'Kana_Shift': [], 'Kanji': [], 'Katakana': [],
+    'Mae_Koho': [], 'Massyo': [], 'Muhenkan': [], 'Romaji': [],
+    'Touroku': [], 'Zen_Koho': [], 'Zenkaku': [], 'Zenkaku_Hankaku': [],
+})
+
+# Maps from literal characters to their key names.
+LITERALS = collections.defaultdict(str, {
+    '`': 'grave', '~': 'asciitilde', '!': 'exclam', '@': 'at',
+    '#': 'numbersign', '$': 'dollar', '%': 'percent', '^': 'asciicircum',
+    '&': 'ampersand', '*': 'asterisk', '(': 'parenleft', ')': 'parenright',
+    '-': 'minus', '_': 'underscore', '=': 'equal', '+': 'plus',
+    '[': 'bracketleft', ']': 'bracketright', '{': 'braceleft',
+    '}': 'braceright', '\\': 'backslash', '|': 'bar', ';': 'semicolon',
+    ':': 'colon', '\'': 'apostrophe', '"': 'quotedbl', ',': 'comma',
+    '<': 'less', '.': 'period', '>': 'greater', '/': 'slash',
+    '?': 'question', '\t': 'Tab', ' ': 'space'
+})
 
 
 class KeyboardCapture(threading.Thread):
@@ -98,93 +252,96 @@ class KeyboardCapture(threading.Thread):
 
 
 class KeyboardEmulation:
-    "Mapping found here: http://msdn.microsoft.com/en-us/library/8c6yea83"
-    ''' Need to test: I can't generate any of these sequences via querty '''
-    keymap_multi = {
-        "Alt_L": "%",
-        "Alt_R": "%",
-        "Control_L": "^",
-        "Control_R": "^",
-        "Shift_L": "+",
-        "Shift_R": "+",
-        }
 
-    keymap_single = {
-        # This library doesn't do anything with just keypresses for alt, ctrl
-        # and shift.
-        "Alt_L": "",
-        "Alt_R": "",
-        "Control_L": "",
-        "Control_R": "",
-        "Shift_L": "",
-        "Shift_R": "",
+ 
+    def key_down(self, keyname):
+        # Press all keys
+        for keycode in KEYNAME_TO_KEYCODE[keyname]:
+                win32api.keybd_event(
+                    keycode,
+                    0,
+                    0,
+                    0
+                )
 
-        "Caps_Lock": "{CAPSLOCK}",
-        "Num_Lock": "{NUMLOCK}",
-        "Scroll_Lock": "{SCROLLLOCK}",
-        "Shift_Lock": "{CAPSLOCK}",  # This is the closest we have.
+    def key_up(self, keyname):
+        # Release all keys backwards
+            for keycode in reversed(KEYNAME_TO_KEYCODE[keyname]):
+                win32api.keybd_event(
+                    keycode,
+                    0,
+                    win32con.KEYEVENTF_KEYUP,
+                    0
+                )
 
-        "Return": "{ENTER}",
-        "Tab": "{TAB}",
-        "BackSpace": "{BS}",
-        "Delete": "{DEL}",
-        "Escape": "{ESC}",
-        "Break": "{BREAK}",
-        "Insert": "{INS}",
-
-        "Down": "{DOWN}",
-        "Up": "{UP}",
-        "Left": "{LEFT}",
-        "Right": "{RIGHT}",
-        "Page_Up": "{PGUP}",
-        "Page_Down": "{PGDN}",
-        "Home": "{HOME}",
-        "End": "{END}",
-
-        "Print": "{PRTSC}",
-        "Help": "{HELP}",
-
-        "F1": "{F1}",
-        "F2": "{F2}",
-        "F3": "{F3}",
-        "F4": "{F4}",
-        "F5": "{F5}",
-        "F6": "{F6}",
-        "F7": "{F7}",
-        "F8": "{F8}",
-        "F9": "{F9}",
-        "F10": "{F10}",
-        "F11": "{F11}",
-        "F12": "{F12}",
-        "F13": "{F13}",
-        "F14": "{F14}",
-        "F15": "{F15}",
-        "F16": "{F16}",
-    }
-
-    SPECIAL_CHARS_PATTERN = re.compile(r'([]{}()+^%~[])')
+    def key_press(self, keyname):
+        self.key_down(keyname)
+        self.key_up(keyname)
     
     def send_backspaces(self, number_of_backspaces):
         for _ in xrange(number_of_backspaces):
-            SendKeys(self.keymap_single['BackSpace'])
+            self.key_press("BackSpace")
 
     def send_string(self, s):
-        SendKeys(re.sub(self.SPECIAL_CHARS_PATTERN, r'{\1}', s))
+        for c in characters(s):
+            if c in LITERALS:
+                c = LITERALS[c]
+            self.key_press(c)
+            
+    def send_key_combination(self, combo_string):
+        """Emulate a sequence of key combinations.
 
-    def send_key_combination(self, s):
-        combo = []
-        tokens = re.split(r'[() ]', s)
-        for token in tokens:
-            if token in self.keymap_multi:
-                combo.append(self.keymap_multi[token])
-            elif token in self.keymap_single:
-                combo.append(self.keymap_single[token])
-            elif token == ' ':
-                pass
+        Argument:
+
+        combo_string -- A string representing a sequence of key
+        combinations. Keys are represented by their names in the
+        KEYNAME_TO_KEYCODE above. For example, the
+        left Alt key is represented by 'Alt_L'. Keys are either
+        separated by a space or a left or right parenthesis.
+        Parentheses must be properly formed in pairs and may be
+        nested. A key immediately followed by a parenthetical
+        indicates that the key is pressed down while all keys enclosed
+        in the parenthetical are pressed and released in turn. For
+        example, Alt_L(Tab) means to hold the left Alt key down, press
+        and release the Tab key, and then release the left Alt key.
+
+        """
+
+        # We will go through and press down keys
+        # When encountering (, we will add to the held stack
+        # ) will release these from the stack
+
+        keycode_events = []
+        key_down_stack = []
+        current_command = []
+        for c in combo_string:
+            if c in (' ', '(', ')'):
+                # Keystring is the variable (l, b, Alt_L, etc.)
+                keystring = ''.join(current_command)
+                # Clear out current command
+                current_command = []
+                if c == ' ':
+                    # Record press and release for command's keys.
+                    self.key_press(keystring)
+                elif c == '(':
+                    # Record press for command's key.
+                    self.key_down(keystring)
+                    key_down_stack.append(keystring)
+                elif c == ')':
+                    # Record press and release for command's key and
+                    # release previously held keys.
+                    self.key_press(keystring)
+                    if key_down_stack:
+                        self.key_up(key_down_stack.pop())
             else:
-                combo.append(token)
-        SendKeys(''.join(combo))
-
+                current_command.append(c)
+        # Record final command key.
+        keystring = ''.join(current_command)
+        self.key_press(keystring)
+        # Release all keys.
+        # Should this be legal in the dict (lack of closing parens)?
+        for keystring in key_down_stack:
+            self.key_up(keystring)
 
 class KeyboardEvent(object):
     """A keyboard event."""
--- windows/DevReadme.txt
+++ windows/DevReadme.txt
@@ -1,16 +1,35 @@
-To create a windows dev environment you need to install:
+Windows Development Environment
+===============================
+
+Dependencies
+-------------
+
+Note: Even if you are developing on a 64-bit system, it is recommended to use a 32-bit tool-chain.
+
+To create a windows development environment you need to install:
+
 - python 2.7
 - pywin32
 - pyserial
-- wxpython for python2.7
+- wxpython
 - appdirs
 - pywinusb
 - mock
 - pyhook
-- pywinauto
+- pyinstaller-2.0 (for building for release)
 
-The starting point for plover is application\plover. You will need to add the root dir of the distribution to the PYTHONPATH environment variable. Alternatively, you can temporarily move application\plover to the root and change its name to launch.py (it may not be called plover.py) and call it from there.
+Configuration
+-------------
 
-To create a windows distribution get pyinstaller-2.0 and run build.bat with the first argument being the path to pyinstaller-2.0 (relative to the working directory).
+To run Plover, in the root of the repository run `python application\plover`.
 
-You have to add a (empty) __init__.py file to the pywinusb dir in site-packages.
+You will need to add the root directory of the Plover repository as the PYTHONPATH environment variable. Alternatively, you can temporarily move `application\plover` to the root and change its name to `launch.py` (it may **not** be called `plover.py`) and run it from there as `python launch.py`.
+
+Building
+---------
+
+Please ensure you have the pyinstaller-2.0 dependency.
+
+You have to add an empty `__init__.py` file to the `%PYTHON27%\Lib\site-packages\pywinusb` directory.
+
+To build a distributable executable, run `build.bat` with the first argument being the path to `pyinstaller-2.0` (relative to the working directory).
